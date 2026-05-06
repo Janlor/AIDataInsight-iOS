d8ee2e5f5094639e555c2ab140d7ac5f44bae7dd
Janlor <JanlorLee@iCloud.com>
2026-05-06 11:03:42 +0800

    项目最低版本改为 iOS 15.0+

--- Packages/library-basics/Package.swift
+++ Packages/library-basics/Package.swift
@@ -6,7 +6,7 @@ let package = Package(
     name: "LibraryBasics",
     defaultLocalization: "en",
     platforms: [
-        .iOS(.v13)
+        .iOS(.v15)
     ],
     products: [
         .library(
